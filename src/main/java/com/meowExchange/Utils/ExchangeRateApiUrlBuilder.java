626733fcba076a315526156da210c3bd4c99e4d5
thnbi <luis.r13@aluno.ifsc.edu.br>
2024-09-24 13:05:02 -0300

    fix: issue with dotenv load in JVM

--- src/main/java/com/meowExchange/Utils/ExchangeRateApiUrlBuilder.java
+++ src/main/java/com/meowExchange/Utils/ExchangeRateApiUrlBuilder.java
@@ -4,8 +4,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class ExchangeRateApiUrlBuilder {
     public static String buildUrl(String currency) {
-        Dotenv dotenv = Dotenv.load();
-        String apiKey = dotenv.get("API_KEY");
+        Dotenv dotenv = Dotenv.configure()
+                .systemProperties()
+                .load();
+        String apiKey = System.getProperty("API_KEY");
+
         return "https://v6.exchangerate-api.com/v6/" +
                 apiKey + "/latest/" +
                 currency.toUpperCase();
